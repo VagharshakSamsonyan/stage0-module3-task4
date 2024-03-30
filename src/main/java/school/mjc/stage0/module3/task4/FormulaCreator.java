8e22cd8f5e7667cbcc48c884a4a70945afb32c42
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-30 22:14:09 +0400

    4 of 5 tests passed

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        // Declare variables
+        int a = 5;
+        int b = 3;
+
+        // Write down the formula
+        int result = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        // Output the result
+        System.out.println("Result: " + result);
     }
 }
--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,22 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char[] chars = {'c', 'h', 'a', 'r'};
+
+        for (char c : chars) {
+            int intValue = (int) c;
+            System.out.print((char) intValue);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
